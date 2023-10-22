68443841c8f5672a44880925286ec0c6ecd795dd
Adam <adam.urban@gmail.com>
2023-10-22 13:32:58 +0200

    chore: updates 2023

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import * as sendgrid from "@sendgrid/mail";
 
 const wichtels = [
   {
-    name: "...",
-    email: "...",
+    name: "",
+    email: "",
+    uuid: "",
   },
 ];
 
@@ -12,7 +13,7 @@ export class WichtelMaster {
 
   constructor() {
     this.randomizeWichtels();
-    console.log(JSON.stringify(this.toSend));
+    console.log(this.toSend.map((w) => `${w.name} -> ${w.wichtel}`).join("\n"));
   }
 
   public async send() {
@@ -43,8 +44,9 @@ export class WichtelMaster {
   }
 
   private randomizeWichtels() {
-    let receipiencts = wichtels;
-    wichtels.forEach((w) => {
+    let receipiencts = this.shuffleArray();
+    let newWichtels = this.shuffleArray();
+    newWichtels.forEach((w) => {
       let wichtel = receipiencts[this.random(0, receipiencts.length - 1)];
       while (w.name === wichtel.name) {
         wichtel = receipiencts[this.random(0, receipiencts.length - 1)];
@@ -60,10 +62,36 @@ export class WichtelMaster {
     });
   }
 
+  private shuffleArray() {
+    const newWichtels = [...wichtels];
+    let currentIndex = newWichtels.length;
+    let randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [newWichtels[currentIndex], newWichtels[randomIndex]] = [
+        newWichtels[randomIndex],
+        newWichtels[currentIndex],
+      ];
+    }
+
+    return newWichtels;
+  }
+
   private random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
 
-const wichtelMaster = new WichtelMaster();
-wichtelMaster.send();
+async function main() {
+  console.log(`Hohoho`);
+  const wichtelMaster = new WichtelMaster();
+  await wichtelMaster.send();
+}
+
+main();
